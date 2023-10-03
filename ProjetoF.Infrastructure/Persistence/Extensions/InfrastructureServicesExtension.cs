7349314c68d695f45a5f02cca5b82ccf65f7ab7a
Luís Felipe <luisrodrigues@bne.com.br>
2023-10-03 19:17:14 -0300

    CHANGE: Refatoração do projeto para Clean Architecture. Detalhamento das entidades Student e Subscription

---
+++ ProjetoF.Infrastructure/Persistence/Extensions/InfrastructureServicesExtension.cs
@@ -0,0 +1,24 @@
+﻿using AspNet_RazorPages.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjetoF.Domain.Entities;
+using ProjetoF.Infrastructure.Persistence.Data;
+using ProjetoF.Infrastructure.Persistence.Repositories;
+
+namespace ProjetoF.Infrastructure.Persistence.Extensions;
+
+internal static partial class InfrastructureServicesExtension
+{
+    public static void ConfigureRepositoriesServices(this IServiceCollection services)
+    {
+        services.AddScoped<IRepository<Student>, Repository<Student>>();
+    }
+
+    public static void ConfigureDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("SQLite");
+        services.AddDbContext<DataContext>(options => options.UseSqlite(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+    }
+}
